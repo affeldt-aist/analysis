6989b8d4a7e90ae8c09d83064c90514995a83fad
AyumuSaito <awe.nun02@gmail.com>
2023-07-10 17:05:54 +0900

    remove about apply

--- theories/lang_syntax.v
+++ theories/lang_syntax.v
@@ -604,7 +604,7 @@ Inductive evalD : forall g t, exp D g t ->
 
 | eval_normalize g t (e : exp P g t) k :
   e -P> k ->
-  exp_normalize e -D> normalize k point ; measurable_mnormalize k
+  [Normalize e] -D> normalize k point ; measurable_mnormalize k
 
 | evalD_if g t e f mf (e1 : exp D g t) f1 mf1 e2 f2 mf2 :
   e -D> f ; mf -> e1 -D> f1 ; mf1 -> e2 -D> f2 ; mf2 ->
@@ -1048,7 +1048,7 @@ Lemma execD_bernoulli g r (r1 : (r%:num <= 1)%R) :
 Proof. exact/execD_evalD/eval_bernoulli. Qed.
 
 Lemma execD_normalize g t (e : exp P g t) :
-  @execD g _ (exp_normalize e) =
+  @execD g _ [Normalize e] =
   existT _ (normalize (execP e) point : _ -> pprobability _ _)
            (measurable_mnormalize (execP e)).
 Proof. exact/execD_evalD/eval_normalize/evalP_execP. Qed.
