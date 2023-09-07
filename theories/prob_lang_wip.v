74f3e4bafb930c64da153cab034d429b1d05ce01
Reynald Affeldt <reynald.affeldt@aist.go.jp>
2023-09-07 17:15:31 +0900

    complete iterate

--- theories/prob_lang_wip.v
+++ theories/prob_lang_wip.v
@@ -9,7 +9,7 @@ Require Import prob_lang.
 
 (******************************************************************************)
 (*  Semantics of a probabilistic programming language using s-finite kernels  *)
-(*                      (wip about gauss density)                             *)
+(*       (wip about definition of Lebesgue and counting measures)             *)
 (******************************************************************************)
 
 Set Implicit Arguments.
@@ -151,3 +151,101 @@ by rewrite integral_indic//= setIid.
 Qed.
 
 End gauss_lebesgue.
+
+(* TODO: move this elsewhere *)
+(* assuming x > 0 *)
+Definition Gamma {R : realType} (x : R) : \bar R :=
+  \int[lebesgue_measure]_(t in `[0%R, +oo[%classic) (expR (- t) * powR t (x - 1))%:E.
+
+Definition Rfact {R : realType} (x : R) := Gamma (x + 1)%R.
+
+Section poisson.
+Variable R : realType.
+Local Open Scope ring_scope.
+Hypothesis integral_poisson_density : forall k,
+  (\int[lebesgue_measure]_x (@poisson R k x)%:E = 1%E)%E.
+
+(* density function for poisson *)
+Definition poisson1 := @poisson R 1%N.
+
+Lemma poisson1_ge0 (x : R) : 0 <= poisson1 x.
+Proof. exact: poisson_ge0. Qed.
+
+Definition mpoisson1 (V : set R) : \bar R :=
+  (\int[lebesgue_measure]_(x in V) (poisson1 x)%:E)%E.
+
+Lemma measurable_fun_poisson1 : measurable_fun setT poisson1.
+Proof. exact: measurable_poisson. Qed.
+
+Let mpoisson10 : mpoisson1 set0 = 0%E.
+Proof. by rewrite /mpoisson1 integral_set0. Qed.
+
+Lemma mpoisson1_ge0 A : (0 <= mpoisson1 A)%E.
+Proof.
+apply: integral_ge0 => x Ax.
+by rewrite lee_fin poisson1_ge0.
+Qed.
+
+Let mpoisson1_sigma_additive : semi_sigma_additive mpoisson1.
+Proof.
+move=> /= F mF tF mUF.
+rewrite /mpoisson1/= integral_bigcup//=; last first.
+  apply/integrableP; split.
+    apply/EFin_measurable_fun.
+    exact: measurable_funS (measurable_poisson _).
+  rewrite (_ : (fun x => _) = (EFin \o poisson1)); last first.
+    by apply/funext => x; rewrite gee0_abs// lee_fin poisson1_ge0//.
+  apply: le_lt_trans.
+    apply: (@subset_integral _ _ _ _ _ setT) => //=.
+      by apply/EFin_measurable_fun; exact: measurable_poisson.
+    by move=> ? _; rewrite lee_fin poisson1_ge0//.
+  by rewrite /= integral_poisson_density// ltry.
+apply: is_cvg_ereal_nneg_natsum_cond => n _ _.
+by apply: integral_ge0 => /= x ?; rewrite lee_fin poisson1_ge0.
+Qed.
+
+HB.instance Definition _ := isMeasure.Build _ _ _
+  mpoisson1 mpoisson10 mpoisson1_ge0 mpoisson1_sigma_additive.
+
+Let mpoisson1_setT : mpoisson1 [set: _] = 1%E.
+Proof.
+rewrite /mpoisson1.
+rewrite /poisson1.
+by rewrite integral_poisson_density.
+Qed.
+
+HB.instance Definition _ := @Measure_isProbability.Build _ _ R mpoisson1 mpoisson1_setT.
+
+Definition poisson' := [the probability _ _ of mpoisson1].
+
+End poisson.
+
+(* Staton's definition of the counting measure
+   Staton ESOP 2017, Sect. 4.2, equation (13)  *)
+Section staton_counting.
+Context d (X : measurableType d).
+Variable R : realType.
+Import Notations.
+Hypothesis integral_poisson_density : forall k,
+  (\int[lebesgue_measure]_x (@poisson R k x)%:E = 1%E)%E.
+
+Let f1 x := (poisson1 (x : R)) ^-1.
+
+Let mf1 : measurable_fun setT f1.
+rewrite /f1 /poisson1 /poisson.
+apply: (measurable_comp (F := [set r : R | r != 0%R])) => //.
+- exact: open_measurable.
+- move=> /= r [t ? <-].
+  by case: ifPn => // t0; rewrite gt_eqF ?mulr_gt0 ?expR_gt0//= invrK ltr0n.
+- apply: open_continuous_measurable_fun => //.
+  by apply/in_setP => x /= x0; exact: inv_continuous.
+- exact: measurable_poisson.
+Qed.
+
+Definition staton_counting : R.-sfker X ~> _ :=
+  letin (sample_cst (@poisson' R integral_poisson_density : pprobability _ _))
+    (letin
+    (score (measurableT_comp mf1 macc1of2))
+    (ret macc1of3)).
+
+End staton_counting.
