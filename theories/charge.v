4d788368b267b9482d6c16aa5f87e21ad3aa5780
IshiguroYoshihiro <jb.15r.1213@s.thers.ac.jp>
2023-11-08 14:21:31 +0900

    jordan_decomp

--- theories/charge.v
+++ theories/charge.v
@@ -876,32 +876,17 @@ Proof. by move=> U mU; rewrite fin_num_measure. Qed.
 HB.instance Definition _ := @Measure_isFinite.Build _ _ _
   jordan_neg finite_jordan_neg.
 
-(*
-Lemma jordan_decomp : nu = [the charge _ _ of (cadd jordan_pos (cscale (-1) jordan_neg))].
-*)
-
-Lemma jordan_decompE (E : set T) (mE : measurable E) : nu E = (cadd jordan_pos (cscale (-1) jordan_neg)) E.
+Lemma jordan_decomp (A : set T) (mA : measurable A) : nu E = (cadd jordan_pos (cscale (-1) jordan_neg)) E.
 Proof.
-rewrite /jordan_pos /jordan_neg/= /measure_of_charge/=.
-rewrite /cscale/= /crestr0/=.
-rewrite charge_addE => /=.
-rewrite -{1}(setIT E).
-have [_ _ PNT _] := nuPN.
-rewrite -PNT.
-rewrite setIUr.
-rewrite charge_semi_additive2.
-        rewrite /cscale/= /crestr0/= mem_set//.
-        rewrite /crestr /= /cscale.
-        rewrite /= muleA.
-        rewrite -EFinM.
-        rewrite mulN1r opprK mul1e.
-        rewrite /crestr0 ifT // /crestr.
-        by rewrite mem_set.
-      by apply: measurableI.
-    by apply: measurableI.
-  by apply: measurableU; apply: measurableI.
-move: nuPN => [_ _ _ PN0].
-by rewrite setIACA PN0 !setI0.
+rewrite /jordan_pos /jordan_neg/= /measure_of_charge/= charge_addE/=.
+rewrite /cscale/= /crestr0/= -[in LHS](setIT E) mem_set//.
+case: nuPN => _ _ <- PN0; rewrite setIUr chargeU.
+- rewrite EFinN mulN1e.
+  rewrite /cscale/= /crestr0/= mem_set//.
+  by rewrite mulN1e oppeK.
+- exact: measurableI.
+- exact: measurableI.
+- by rewrite setIACA PN0 setI0.
 Qed.
 
 Lemma jordan_pos_dominates (mu : {measure set T -> \bar R}) :
