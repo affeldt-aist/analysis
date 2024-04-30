b7e4bac9a07776c0a92ce9cc3aea5005fd399b4e
IshiguroYoshihiro <jb.15r.1213@s.thers.ac.jp>
2024-04-30 17:34:20 +0900

    lebesgue_stieltjes_measure_bounded_itv

--- theories/FTC_for_continuous.v
+++ theories/FTC_for_continuous.v
@@ -16,6 +16,175 @@ Import numFieldTopology.Exports.
 Local Open Scope classical_set_scope.
 Local Open Scope ring_scope.
 
+Section lebesgue_stieltjes_measure_itv.
+Variable R : realType.
+Variable f : cumulative R.
+
+Let lebesgue_stieltjes_measure_itvoc (a b : R) :
+  (lebesgue_stieltjes_measure f (`]a, b] : set R) =
+  wlength f `]a, b])%classic.
+Proof.
+rewrite /lebesgue_measure/= /lebesgue_stieltjes_measure/= /measure_extension/=.
+by rewrite measurable_mu_extE//; exact: is_ocitv.
+Qed.
+
+Let lebesgue_stieltjes_measure_itvoo_subr1 (a : R) :
+  lebesgue_stieltjes_measure f (`]a - 1, a[%classic : set R)
+     = ((f a)%:E - (f (a - 1))%:E)%E.
+Proof.
+rewrite itv_bnd_open_bigcup//; transitivity (limn ((lebesgue_stieltjes_measure f)
+ \o (fun k => `]a - 1, a - k.+1%:R^-1]%classic : set R))).
+  apply/esym/cvg_lim => //; apply: nondecreasing_cvg_mu.
+  - by move=> ?; exact: measurable_itv.
+  - by apply: bigcup_measurable => k _; exact: measurable_itv.
+  - move=> n m nm; apply/subsetPset => x /=; rewrite !in_itv/= => /andP[->/=].
+    by move/le_trans; apply; rewrite lerB// ler_pV2 ?ler_nat//;
+      rewrite inE ltr0n andbT unitfE.
+(* right continuity of f *)
+Admitted.
+
+Lemma lebesgue_stieltjes_measure_set1 (a : R) : lebesgue_stieltjes_measure f [set a] = 0%E.
+Proof.
+suff : (lebesgue_stieltjes_measure f (`]a - 1, a]%classic%R : set R) =
+        lebesgue_stieltjes_measure f (`]a - 1, a[%classic%R : set R) +
+        lebesgue_stieltjes_measure f [set a])%E.
+  rewrite lebesgue_stieltjes_measure_itvoo_subr1 lebesgue_stieltjes_measure_itvoc => /eqP.
+  rewrite wlength_itv lte_fin ltrBlDr ltrDl ltr01.
+  rewrite [in X in X == _]/= EFinN -[X in _ == X]addeC.
+  by rewrite -sube_eq ?fin_num_adde_defl// subee// => /eqP.
+rewrite -setUitv1// ?bnd_simp; last by rewrite ltrBlDr ltrDl.
+rewrite measureU //; apply/seteqP; split => // x []/=.
+by rewrite in_itv/= => + xa; rewrite xa ltxx andbF.
+Qed.
+
+Let lebesgue_stieltjes_measure_itvoo (a b : R) :
+  (lebesgue_stieltjes_measure f (`]a, b[ : set R) =
+   wlength f `]a, b[)%classic.
+Proof.
+have [ab|ba] := ltP a b; last by rewrite set_itv_ge ?measure0// -leNgt.
+have := lebesgue_stieltjes_measure_itvoc a b.
+rewrite 2!wlength_itv => <-; rewrite -setUitv1// measureU//.
+- by have /= -> := lebesgue_stieltjes_measure_set1 b; rewrite adde0.
+- by apply/seteqP; split => // x [/= + xb]; rewrite in_itv/= xb ltxx andbF.
+Qed.
+
+Let lebesgue_stieltjes_measure_itvcc (a b : R) :
+  (lebesgue_stieltjes_measure f (`[a, b] : set R) =
+   wlength f `[a, b])%classic.
+Proof.
+have [ab|ba] := leP a b; last by rewrite set_itv_ge ?measure0// -leNgt.
+have := lebesgue_stieltjes_measure_itvoc a b.
+rewrite 2!wlength_itv => <-; rewrite -setU1itv// measureU//.
+- by have /= -> := lebesgue_stieltjes_measure_set1 a; rewrite add0e.
+- by apply/seteqP; split => // x [/= ->]; rewrite in_itv/= ltxx.
+Qed.
+
+Let lebesgue_stieltjes_measure_itvco (a b : R) :
+  (lebesgue_stieltjes_measure f (`[a, b[ : set R) =
+   wlength f `[a, b[)%classic.
+Proof.
+have [ab|ba] := ltP a b; last by rewrite set_itv_ge ?measure0// -leNgt.
+have := lebesgue_stieltjes_measure_itvoo a b.
+rewrite 2!wlength_itv => <-; rewrite -setU1itv// measureU//.
+- by have /= -> := lebesgue_stieltjes_measure_set1 a; rewrite add0e.
+- by apply/seteqP; split => // x [/= ->]; rewrite in_itv/= ltxx.
+Qed.
+
+Let lebesgue_stieltjes_measure_itv_bnd (x y : bool) (a b : R) :
+  lebesgue_stieltjes_measure f ([set` Interval (BSide x a) (BSide y b)] : set R) =
+  wlength f [set` Interval (BSide x a) (BSide y b)].
+Proof.
+by move: x y => [|] [|]; [exact: lebesgue_stieltjes_measure_itvco |
+  exact: lebesgue_stieltjes_measure_itvcc |
+  exact: lebesgue_stieltjes_measure_itvoo |
+  exact: lebesgue_stieltjes_measure_itvoc].
+Qed.
+
+(* (* conflict to wlength_itv *) *)
+(* (* do after remove er_map in definition of wlength *) *)
+
+(* (* Let cumulative_limnatR : lim (((f (k%:R))%:E : \bar R) @[k --> \oo]) = +oo%E. *) *)
+(* (* Proof. apply/cvg_lim => //. ; apply/cvgenyP. Qed. *) *)
+
+(* Let lebesgue_stieltjes_measure_itv_bnd_infty x (a : R) : *)
+(*   lebesgue_stieltjes_measure f ([set` Interval (BSide x a) +oo%O] : set R) = *)
+(*      (lim (((f (a + k%:R))%:E : \bar R) @[k --> \oo]) - (f a)%:E)%E. *)
+(* Proof. *)
+(* rewrite itv_bnd_infty_bigcup; transitivity (limn ((lebesgue_stieltjes_measure f) \o *)
+(*     (fun k => [set` Interval (BSide x a) (BRight (a + k%:R))] : set R))). *)
+(*   apply/esym/cvg_lim => //; apply: nondecreasing_cvg_mu. *)
+(*   + by move=> k; exact: measurable_itv. *)
+(*   + by apply: bigcup_measurable => k _; exact: measurable_itv. *)
+(*   + move=> m n mn; apply/subsetPset => r/=; rewrite !in_itv/= => /andP[->/=]. *)
+(*     by move=> /le_trans; apply; rewrite lerD// ler_nat. *)
+(* rewrite (_ : _ \o _ = (fun k => (f (a + k%:R) - f a)%:E)). *)
+(*   admit. *)
+(* apply/funext => n /=; rewrite lebesgue_stieltjes_measure_itv_bnd wlength_itv/=. *)
+(* rewrite lte_fin;  have [->|n0] := eqVneq n 0%N; last first. *)
+(*   rewrite EFinD ifT //. *)
+(*   admit. *)
+(* by rewrite ifF addr0 ?subrr. *)
+(* Admitted. *)
+
+(* Let lebesgue_stieltjes_measure_itv_infty_bnd y (b : R) : *)
+(*   lebesgue_stieltjes_measure f ([set` Interval -oo%O (BSide y b)] : set R) = *)
+(*     ((f b)%:E - lim (((f (b - k%:R))%:E : \bar R) @[k --> \oo]))%E. *)
+(* Proof. *)
+(* rewrite itv_infty_bnd_bigcup; transitivity (limn ((lebesgue_stieltjes_measure f) \o *)
+(*     (fun k => [set` Interval (BLeft (b - k%:R)) (BSide y b)] : set R))). *)
+(*   apply/esym/cvg_lim => //; apply: nondecreasing_cvg_mu. *)
+(*   + by move=> k; exact: measurable_itv. *)
+(*   + by apply: bigcup_measurable => k _; exact: measurable_itv. *)
+(*   + move=> m n mn; apply/subsetPset => r/=; rewrite !in_itv/= => /andP[+ ->]. *)
+(*     by rewrite andbT; apply: le_trans; rewrite lerB// ler_nat. *)
+(* rewrite (_ : _ \o _ = (fun k : nat => ((f b)%:E) - (f (b - k%:R))%:E)%E). *)
+(*   admit. *)
+(* apply/funext => n /=; rewrite lebesgue_stieltjes_measure_itv_bnd wlength_itv/= lte_fin. *)
+(* have [->|n0] := eqVneq n 0%N; last first. *)
+(*   rewrite ifT //. *)
+(*   admit. *)
+(* by rewrite ifF ?subr0 // -EFinD subrr. *)
+(* Admitted. *)
+
+(* Let lebesgue_stieltjes_measure_itv_infty_infty : *)
+(*   lebesgue_stieltjes_measure f ([set` Interval -oo%O +oo%O] : set R) = *)
+(*     (lim (((f k%:R)%:E : \bar R) @[k --> \oo]) - lim (((f (- k%:R))%:E : \bar R) @[k --> \oo]))%E. *)
+(* Proof. *)
+(* rewrite set_itv_infty_infty -(setUv (`]-oo, 0[)) setCitv. *)
+(* rewrite [X in _ `|` (X `|` _) ]set_itvE set0U measureU//; last first. *)
+(*   apply/seteqP; split => //= x [] /= /[swap]. *)
+(*   by rewrite !in_itv/= andbT ltNge => ->//. *)
+(* rewrite [X in (X + _)%E]lebesgue_stieltjes_measure_itv_infty_bnd. *)
+(* rewrite [X in (_ + X)%E]lebesgue_stieltjes_measure_itv_bnd_infty. *)
+(* under eq_fun do rewrite sub0r. *)
+(* rewrite addeCA. *)
+(* under eq_fun do rewrite add0r. *)
+(* by rewrite addeAC subee // sub0e. *)
+(* Qed. *)
+
+(* (* Lemma lebesgue_stieltjes_measure_itv (i : interval R) : *) *)
+(* (*   lebesgue_stieltjes_measure f ([set` i] : set R) = *) *)
+(* (*   (if i.1 < i.2 then (f (i.2 : R) - (f (i.1 : R)))%E else 0)%E. *) *)
+(* (* Proof. *) *)
+(* (* move: i => [[x a|[|]]] [y b|[|]]. *) *)
+(* (*   by rewrite lebesgue_measure_itv_bnd wlength_itv. *) *)
+(* (* - by rewrite set_itvE ?measure0. *) *)
+(* (* - by rewrite lebesgue_measure_itv_bnd_infty/= ltry. *) *)
+(* (* - by rewrite lebesgue_measure_itv_infty_bnd/= ltNyr. *) *)
+(* (* - by rewrite set_itvE ?measure0. *) *)
+(* (* - by rewrite lebesgue_measure_itv_infty_infty. *) *)
+(* (* - by rewrite set_itvE ?measure0. *) *)
+(* (* - by rewrite set_itvE ?measure0. *) *)
+(* (* - by rewrite set_itvE ?measure0. *) *)
+(* (* Qed. *) *)
+
+Lemma lebesgue_stieltjes_measure_bounded_itv (x y : R) (b b' : bool) :
+  lebesgue_stieltjes_measure f [set` (Interval (BSide b x) (BSide b' y))] =
+    (if (x < y)%R then (f y - f x)%:E else 0)%E.
+Proof. by rewrite lebesgue_stieltjes_measure_itv_bnd wlength_itv. Qed.
+
+End lebesgue_stieltjes_measure_itv.
+
 Section drestrict.
 Context (R : realType).
 
@@ -104,4 +273,7 @@ rewrite -[LHS]mul1e.
 rewrite -integral_cst /=.
 rewrite (_: (\int[mu]_(_ in [set f x | x in `]a, b[]) 1)%E  = 
   (\int[mu]_(x in `]a, b[) dlafdmu x)%E).
+rewrite -Radon_Nikodym_integral /=.
+rewrite /lebesgue_stieltjes_measure /=.
+rewrite wlength_itv.
 Admitted.
