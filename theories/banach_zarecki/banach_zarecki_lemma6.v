c868606e315102faa6b5ea346dcfc6988f4e17c8
Reynald Affeldt <reynald.affeldt@aist.go.jp>
2025-11-06 15:04:22 +0900

    wip (lemma 6)

--- theories/banach_zarecki/banach_zarecki_lemma6.v
+++ theories/banach_zarecki/banach_zarecki_lemma6.v
@@ -4,8 +4,11 @@ From mathcomp Require Import all_ssreflect interval_inference ssralg ssrnum.
 From mathcomp Require Import ssrint interval archimedean.
 From mathcomp Require Import mathcomp_extra boolp classical_sets functions.
 From mathcomp Require Import reals constructive_ereal topology normedtype.
+From mathcomp Require Import ereal sequences.
 From mathcomp Require Import measure lebesgue_measure realfun.
-From mathcomp Require Import absolute_continuity banach_zarecki_lemma3.
+From mathcomp Require Import absolute_continuity banach_zarecki_lemma2.
+From mathcomp Require Import banach_zarecki_lemma3.
+From mathcomp Require Import banach_zarecki_lemma4 (* for contiguous intervals *).
 
 (**md**************************************************************************)
 (* # Banach–Zarecki Theorem (lemma 6)                                         *)
@@ -56,39 +59,48 @@ move/contra_not => /(_ ababsurdo).
 move/existsNP => [Z /not_implyP [Zab /not_implyP[cZ /not_implyP[muZ0]]]].
 move/eqP; rewrite neq_lt ltNge measure_ge0/= => muHZ_gt0.
 have compactH : compact (H @` Z).
-  have := @continuous_compact _ _ H Z.
-  apply.
-    apply: continuous_subspaceW.
-      exact: Zab.
-    assumption.
+  apply: (@continuous_compact _ _ H Z).
+    exact: (continuous_subspaceW Zab).
   exact: cZ.
 pose c : R := inf Z.
 pose d : R := sup Z.
-wlog : Z Zab cZ muZ0 muHZ_gt0 compactH c d / perfect_set Z.
+have perfectZ : perfect_set Z.
+  admit. (* wlog *)
+pose ab_ := contiguous_intervals Z.
+pose alpha := mu (H @` Z).
+have alpha_ge0 : (alpha > 0)%E by [].
+pose c_ (n : nat) (i : 'I_n.+1) :=
+  if nat_of_ord i == 0 then c else fine (contiguous_intervals2 Z i).
+pose d_ (n : nat) (i : 'I_n.+1) :=
+  if i == @ord_max n then d else fine (contiguous_intervals1 Z i).
+pose lambda (n : nat) := \big[Num.max/0%R]_(i < n.+1) (d_ _ i - c_ _ i).
+have lambda_cvg0 : lambda n @[n --> \oo] --> 0.
   admit.
-move=> perfectZ.
-
-pose TV := (fine \o (total_variation a)^~ f).
-have : exists n : nat, (0 < n)%N /\ exists Z_ : `I_ n -> interval R, trivIset setT (fun i => [set` (Z_ i)])
-   /\ (0 < mu (TV @` (\bigcup_i [set` Z_ i])))%E
-   /\ forall i, [/\ [set` Z_ i] `<=` `[a, b], compact [set` Z_ i] & mu [set` Z_ i] = 0].
+have [x_ cdx_] : exists x_ : seq R, itv_partition c d x_ (* NB: p = size (c :: x_) *).
   admit.
-move=> [n [] n0 [Z_]] [trivZ [Uz0]] /all_and3 [Zab' cZ' Z0].
-pose UZ := \bigcup_i [set` Z_ i].
-have UZ_not_empty : UZ !=set0.
+pose X_ := c :: x_.
+pose p := size X_.
+pose S_ (n : nat) := \sum_(1 <= j < p.+1) `|f (nth 0 X_ j) - f (nth 0 X_ (j - 1))|.
+pose V_ (n : nat) := \sum_(1 <= i < n.+1) `|f (d_ n (inord i)) - f (c_ n (inord i))|
+           +
+           \sum_(1 <= i < n) fine (total_variation (fine (contiguous_intervals1 Z i))
+                                             (fine (contiguous_intervals2 Z i))
+                                             f).
+pose V := fine (total_variation c d f).
+have S_V_V (n : nat) : S_ n <= V_ n <= V.
   admit.
-pose l_ i : R := inf [set` Z_ i].
-pose r_ i : R := sup [set` Z_ i].
-pose alpha := mu [set (fine \o (total_variation a)^~ f) x | x in UZ].
-have rct : right_continuous TV.
+have S_V : S_ n @[n --> \oo] --> V.
   admit.
-have monot : {in `[a, b]&, {homo TV : x y / x <= y}}.
+have V_V : V_ n @[n --> \oo]--> V.
   admit.
-(*
-have : exists n, exists I : (R * R)^nat,
- [/\ (forall i, (I i).1 < (I i).2 /\ `[(I i).1, (I i).2] `<=` `[a, b] ),
-     trivIset setT (fun i => `[(I i).1, (I i).2]%classic) &
-     \bigcup_(0 <= i < n) (`[(I i).1, (I i).2]%classic) = Z].*)
+have [n0 Hn0] : exists n0, forall n, (n >= n0)%N -> V_ n > V - (fine alpha) / 2.
+  admit.
+have H1 (n : nat) : V = \sum_(1 <= i < n.+1) `|H (d_ n (inord i)) - H (c_ n (inord i))|
+           +
+           \sum_(1 <= i < n) fine (total_variation (fine (contiguous_intervals1 Z i))
+                                             (fine (contiguous_intervals2 Z i))
+                                             f).
+  admit.
 Admitted.
 
 End lemma6_direct.
@@ -100,13 +112,77 @@ Hypotheses ab : a < b.
 
 Local Notation mu := (@completed_lebesgue_measure R).
 
+Variable f : R -> R.
+
+Let H := fun x => fine (total_variation a ^~ f x).
+
 (* lemma6(i) *)
-Lemma total_variation_Lusin (f : R -> R) :
+Lemma total_variation_Lusin :
   {within `[a, b], continuous f} ->
   bounded_variation a b f ->
-  lusinN `[a, b] (fun x => fine ((total_variation a ^~ f) x)) ->
-  lusinN `[a, b] f.
+  lusinN `[a, b] H -> lusinN `[a, b] f.
 Proof.
+move=> cf abf.
+move=> lusinNH Z Zab/= mZ mZ0.
+have muZ_lty : ((wlength idfun)^*%mu Z < +oo)%E.
+  move: mZ0.
+  rewrite /mu/=.
+  (* TODO: lemma to avoid unfold *)
+  rewrite /completed_lebesgue_stieltjes_measure.
+  by rewrite /completed_measure_extension => ->.
+move : muZ_lty => /(@lebesgue_measure_Gdelta_approx R Z)[G [ZG oG Gnonincreasing muZ]].
+pose Z1 := `]a, b[ `&` \bigcap_i G i.
+suff: mu (f @` Z1) = 0.
+  move=> mfZ1.
+  apply/eqP; rewrite eq_le measure_ge0 andbT.
+  rewrite -mfZ1.
+  rewrite le_outer_measure//.
+  rewrite /Z1.
+
+  apply: image_subset.
+  rewrite -bigcapIr//.
+  apply: sub_bigcap => i _.
+  rewrite subsetI; split => //.
+  admit.
+have H1 : mu (H @` Z1) = mu (\bigcap_i (H @` (G i))) /\
+       mu (\bigcap_i (H @` (G i))) = 0.
+  split.
+    rewrite completed_lebesgue_measureE.
+    have := @measure_image_nondecreasing_fun R a b H ab _ G.
+    (* mismatch Z1 should be an intersection of G's... *)
+    admit.
+
+(*      rewrite fine_le//.
+      + apply/bounded_variationP => //.
+        exact: bounded_variationl abf.
+      + apply/bounded_variationP => //.
+        exact: bounded_variationl abf.
+      + apply: (total_variation_nondecreasing f) => //; rewrite ?in_itv/=.
+        by rewrite xa/=; exact: xb.
+        by rewrite ya/=; exact: yb.
+    - exact: total_variation_continuous.
+    - move=> k.*)
+
+  admit.
+have H2 : mu (H @` G i) @[i --> \oo] --> 0%E.
+  admit.
+pose G_ i := \bigcup_j (open_disjointI (oG i) j).
+have H3 i :
+  mu (f @` Z1) = mu (f @` (\bigcup_j (Z1 `&` (open_disjointI (oG i) j)))).
+  admit.
+have H4 i :
+    (mu (f @` (\bigcup_j (Z1 `&` (open_disjointI (oG i) j)))) <=
+    \sum_(j <oo) (mu^*)%mu (f @` (Z1 `&` (open_disjointI (oG i) j))))%E.
+  admit.
+have H5 i :
+    (\sum_(j <oo) (mu^*)%mu (f @` (Z1 `&` (open_disjointI (oG i) j))) <
+    \sum_(j <oo) oscillation f (closure (open_disjointI (oG i) j)))%E.
+  admit.
+have H6 i :
+    (\sum_(j <oo) oscillation f (closure (open_disjointI (oG i) j)) =
+    mu (H @` G_ i))%E.
+  admit.
+apply/eqP; rewrite eq_le measure_ge0 andbT.
 Admitted.
 
 End lemma6_converse.
