2c8cbb459d0437aaf6a4ab87e1f5221cf6c72da6
IshiguroYoshihiro <jb.15r.1213@s.thers.ac.jp>
2025-11-07 17:47:06 +0900

    wip variation_merge

--- theories/banach_zarecki/banach_zarecki_lemma5.v
+++ theories/banach_zarecki/banach_zarecki_lemma5.v
@@ -27,34 +27,109 @@ Section itv_partition_length.
 Context {R : realType}.
 Implicit Types (a b : R) (f : R -> R).
 
-Definition itv_partition_length a b s : R := let pnth := nth b (a :: s) in
+Definition itv_partition_max a b s : R := let pnth := nth b (a :: s) in
   \big[maxr/0%R]_(0 <= n < size s) `|pnth n.+1 - pnth n|%R.
 
-Definition itv_partition_set a b l : set (seq R) := [set s | itv_partition a b s /\
-  itv_partition_length a b s < l].
+Definition itv_partition_with_max a b l s :=
+  itv_partition a b s /\ itv_partition_max a b s = l.
+
+Definition variations_with_max a b f l : set R :=
+   [set variation a b f s | s in itv_partition_with_max a b l].
+
+Definition omega_max a b f s : \bar R :=
+   \big[maxe/0%E]_(0 <= n < size s) oscillation f
+    `[(nth b (a :: s)) n, (nth b (a :: s)) n.+1].
 
 End itv_partition_length.
 
 Section lemma5.
 Context {R : realType}.
-Implicit Types (a b : R) (f : R -> R).
+Variables (a b : R) (f : R -> R).
+Hypothesis (ab : a < b).
 
-Lemma lemma5' a b f :
-  {within `[a, b], continuous f} ->
-  forall A : R, (A%:E < total_variation a b f)%E ->
-    exists l p, [/\ itv_partition a b p,
-              itv_partition_length a b p < l,
-              A < variation a b f p &
-              ((variation a b f p)%:E < total_variation a b f)%E].
+Arguments unif_continuous : clear implicits.
+
+Let variation_merge1 s :
+  itv_partition a b s -> (* not necessary? *)
+  forall x, x \in `[a, b] ->
+    ((variation a b f (merge <%R s [:: x]))%:E <=
+          (variation a b f s)%:E + 2 * omega_max a b f s)%E.
 Proof.
+move=> parts.
+move=> x; rewrite in_itv/= => /andP[ax xb].
+rewrite (@in_itv_partition _ x (merge <%R s [:: x])); last 2 first.
+- admit.
+- admit.
+(* rewrite variation_cat. *)
+Admitted.
+
+Let variation_merge l s t : itv_partition_with_max a b l s ->
+  itv_partition a b t ->
+  ((variation a b f (merge <%R s t))%:E <= (variation a b f s)%:E +
+  (size t)%:R%:E * 2 *omega_max a b f s)%E.
+Proof.
+Admitted.
+
+Lemma lemma5' :
+  {within `[a, b], continuous f} ->
+  bounded_variation a b f ->
+  forall A : R, (0%:E < A%:E < total_variation a b f)%E ->
+    exists l, forall p, itv_partition_with_max a b l p ->
+              A < variation a b f p.
+Proof.
+move=> cf.
+move/(bounded_variationP f (ltW ab)) => bvf.
+move=> A /andP[]; rewrite lte_fin => A0.
+set Tf : \bar R := total_variation a b f.
+rewrite /Tf/total_variation.
+move=> ATf.
+have TfA0 : 0 < fine Tf - A.
+  admit.
+have [eV' /= [V' [X' partX' X'V'] V'eV']] := ub_ereal_sup_adherent TfA0 bvf.
+rewrite -/(total_variation a b f) -/Tf.
+rewrite EFinN EFinB fineK//.
+rewrite oppeB; last first.
+  by rewrite fin_num_adde_defl.
+rewrite addeA subee// add0r.
+rewrite -{}V'eV' lte_fin => AV'.
+have : unif_continuous (subspace `[a, b]) R f.
+  admit.
+move/unif_continuousP => /=.
+pose m := size X'.
+pose eps := ((V' - A) / (4 * m)%:R).
+have eps0 : 0 < eps.
+  admit.
+move/(_ _ eps0) => [d d0 unifcf].
+exists d => p [partp pmaxd].
+
+
+
+
+have : [set x%:E | x in variations a b f] != set0.
+  admit.
+move/ereal_sup_seq => [v'_].
+rewrite /=.
+under [X in X -> _]eq_forall do rewrite exists2E.
+move/choice => [v_] /all_and2 [+ vv'].
+rewrite /variations/=.
+under [X in X -> _]eq_forall do rewrite exists2E.
+move/choice=> [p_] /all_and2[partp pv].
+under eq_cvg => x do rewrite -vv' -pv.
+rewrite cvg_EFin.
+  over.
 Abort.
 
 Lemma lemma5 a b f :
   {within `[a, b], continuous f} ->
   ereal_inf
-     [set (variation a b f s)%:E | s in itv_partition_set a b l] @[l --> 0^'+]
+     [set v%:E | s in variations_with_max a b f l] @[l --> 0^'+]
        --> total_variation a b f.
 Proof.
+move=> cf.
+rewrite /total_variation.
+rewrite /variations.
+rewrite image_comp.
+apply/cvgrPdist_lt.
 Abort.
 
 End lemma5.
